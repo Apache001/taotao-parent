c500888116234f229ef66f090849e14267bfa625
wangpz <751920167@qq.com>
2018-08-11 09:02:34 +0800

    solr搜索

---
+++ taotao-search/taotao-search-interface/src/main/java/com/taotao/search/service/SearchItemService.java
@@ -0,0 +1,12 @@
+package com.taotao.search.service;
+
+import com.taotao.common.pojo.TaotaoResult;
+
+/**
+ * @Author: wpz
+ * @Desctription:
+ * @Date: Created in 2018/8/8 21:58
+ */
+public interface SearchItemService {
+    public TaotaoResult importItemsToIndex();
+}
---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/mapper/SearchItemMapper.java
@@ -0,0 +1,14 @@
+package com.taotao.search.mapper;
+
+import com.taotao.common.pojo.SearchItem;
+
+import java.util.List;
+
+/**
+ * @Author: wpz
+ * @Desctription:
+ * @Date: Created in 2018/8/8 21:49
+ */
+public interface SearchItemMapper {
+    List<SearchItem> getItemList();
+}
